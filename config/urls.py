c8c074ea1f7be9975439ba99e197c0a8cb871669
Donghyeonkim981214 <43703346+Donghyeonkim981214@users.noreply.github.com>
2021-05-07 04:05:46 +0900

    final modify before deploy

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +23,5 @@ urlpatterns = [
     path("list_api/", include("lists.urls", namespace='list_api')),
     path("card_api/", include("cards.urls", namespace='card_api')),
     path("users_api/", include('users.urls')),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
